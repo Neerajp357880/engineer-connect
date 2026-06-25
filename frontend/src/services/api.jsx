0d4ab0d8dd687a7afb13a06230fb2b99b676f16d
Neeraj Pandey <neerajpandey357880@gmail.com>
2026-06-25 18:05:51 +0545

    Fixed API URL

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://engineer-connect-ekqx.onrender.com",
+  baseURL:  "https://engineer-connect-ekqx.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
